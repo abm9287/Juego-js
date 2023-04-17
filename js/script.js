223e9fdbedadfcd8f351b0b6d6335dbe7627b342
Ana Belen Chamba Mera <abm.chamba@gmail.com>
2023-04-17 12:19:50 -0500

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,47 +1,37 @@
-function aleatorio(min, max){
-    return Math.floor(Math.random() * (max - min + 1)+ min) ;
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function eleccion(jugada) {
+    let resultado = ""; 
+    if (jugada == 1) {
+        resultado = 'Piedra 🥌';
+    } else if (jugada == 2) {
+        resultado = 'Papel 📄'
+    } else if (jugada == 3) {
+        resultado = 'Tijeras ✂';
+    } else {
+        resultado = 'Haz elegido mal❌';
+    }
+    return resultado;
+}
+
 let jugador = 0;
-let pc = aleatorio(1,3);
+let pc = aleatorio(1, 3);
 jugador = prompt("Elige: \n1.Piedra 🥌 \n2.Papel📖 \n3.Tijera✂");
 
-//JUGADOR
-if(jugador == 1){
-    alert('Elegiste 🥌');
-}else if(jugador == 2){
-    alert('Elegiste 📄')
-}else if(jugador == 3){
-    alert('Elegiste ✂')
-}else{
-    alert(`Haz elegido mal`)
-}
-
-//PC
-if(pc == 1){
-    alert('Elegiste 🥌')
-}else if(pc == 2){
-    alert('Elgiste 📄')
-}else if(pc == 3){
-    alert('Elegiste ✂')
-}
+alert(`PC elige: ${eleccion(pc)}`);
+alert(`Tu elijes: ${eleccion(jugador)}`)
 
 //Combate
-/*if(pc == jugador){
-    alert('Empate')
-}else if(jugador == 1 && pc == 3){
-    alert('Ganaste!');
-}else if(jugador == 2 && pc == 1){
-    alert('Ganaste!');
-}else if(jugador == 3 && pc == 2){
-    alert('Ganaste!');
-}else{
-    alert('Perdiste');
-}*/
-
 if(pc == jugador){
-    alert('Empate💯💪😄')
-}else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
-    alert('Ganaste🏆');
+    alert('Empate✅')
+}else if(jugador == 1 && pc == 3){
+    alert('Ganaste!🏆');
+}else if(jugador == 2 && pc == 1){
+    alert('Ganaste!🏆');
+}else if(jugador == 3 && pc == 2){
+    alert('Ganaste!🏆');
 }else{
-    alert('Perdiste❌😫');
+    alert('Perdiste😫');
 }
